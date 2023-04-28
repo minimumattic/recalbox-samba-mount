27145fd076b8b4f8ae64022cb27216039a51a120
BlackOp <blackop@disroot.org>
2023-04-28 15:16:42 +0300

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PARAMS="-o credentials=/recalbox/share/system/.smbcredentials,uid=0,gid=0,noperm,ro"
-LAN="//192.168.1.10/Games/roms"   #change this with your samba shared ip address and roms directory.
+LAN="//192.168.1.10/Games/roms"   # change this with your samba shared ip address and roms directory.
 RBROMS="/recalbox/share/roms"
 
 if [ $1 == "start" ]
